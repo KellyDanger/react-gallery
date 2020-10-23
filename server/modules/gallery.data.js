7c1dd674211c4f14e5bf5c621be163c81e4438b0
Kelly Danger <kellyanndanger@gmail.com>
2020-10-23 16:52:33 -0500

    get route talking to server working on postman

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: 'images/AylaDragon.jpeg', description: 'Photo of a child in a dragon costume.', likes: 0 },
+    { id: 1, path: 'images/cat_lick.jpeg', description: "Photo of a cat tucked in a sweather licking it's lips", likes: 0 },
+    { id: 1, path: 'images/fall_trees.jpeg', description: 'Photo of autumnal trees.', likes: 0 },
+    { id: 1, path: 'images/K_climb.jpg', description: 'Photo of a woman climbing a bouldering wall.', likes: 0 },
+    { id: 1, path: 'images/morning_glory.jpg', description: 'Photo of a deep purple morning glory flower.', likes: 0 },
+    { id: 1, path: 'images/Rue_climb.jpg', description: 'Photo of a child climbing a bouldering wall.', likes: 0 },
+    
 ];
 
 module.exports = galleryItems;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,19 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  getGallery = () => {
+    axios({
+      method: 'GET',
+      url: '/songs'
+    }).then((response) => {
+      console.log('response from GET', response.data);
+    }).catch((error) => {
+      console.log('error in GET', error);  
+    })
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +22,7 @@ class App extends Component {
         </header>
         <br/>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        <img src="images/goat_small.jpg" alt="goat"/>
       </div>
     );
   }
