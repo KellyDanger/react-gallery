5dbc3d439d31550bad9c7063e30b373b43c2af1f
Kelly Danger <kellyanndanger@gmail.com>
2020-10-25 10:11:47 -0500

    added database

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,12 +1,12 @@
-const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0},
-    { id: 2, path: 'images/AylaDragon.jpeg', description: 'Photo of a child in a dragon costume.', likes: 0 },
-    { id: 3, path: 'images/cat_lick.jpeg', description: "Photo of a cat tucked in a sweather licking it's lips", likes: 0 },
-    { id: 4, path: 'images/fall_trees.jpeg', description: 'Photo of autumnal trees.', likes: 0 },
-    { id: 5, path: 'images/K_climb.jpeg', description: 'Photo of a woman climbing a bouldering wall.', likes: 0 },
-    { id: 6, path: 'images/morning_glory.jpeg', description: 'Photo of a deep purple morning glory flower.', likes: 0 },
-    { id: 7, path: 'images/Rue_climb.jpeg', description: 'Photo of a child climbing a bouldering wall.', likes: 0 },
+// const galleryItems = [
+//     // { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0},
+//     { id: 2, path: 'images/AylaDragon.jpeg', description: 'Photo of a child in a dragon costume.', likes: 0 },
+//     { id: 3, path: 'images/cat_lick.jpeg', description: "Photo of a cat tucked in a sweather licking it's lips", likes: 0 },
+//     { id: 4, path: 'images/fall_trees.jpeg', description: 'Photo of autumnal trees.', likes: 0 },
+//     { id: 5, path: 'images/K_climb.jpeg', description: 'Photo of a woman climbing a bouldering wall.', likes: 0 },
+//     { id: 6, path: 'images/morning_glory.jpeg', description: 'Photo of a deep purple morning glory flower.', likes: 0 },
+//     { id: 7, path: 'images/Rue_climb.jpeg', description: 'Photo of a child climbing a bouldering wall.', likes: 0 },
     
-];
+// ];
 
-module.exports = galleryItems;
+// module.exports = galleryItems;
